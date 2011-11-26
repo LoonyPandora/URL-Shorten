9ea8186d8597f49164daa111b9129236b2e930ed
James Aitken <loonypandora@gmail.com>
2011-11-26 02:47:48 +0000

    Create an interface for shortening.

--- demo.pl
+++ demo.pl
@@ -2,11 +2,53 @@
 
 use common::sense;
 use lib 'lib';
-use URL::Shorten;
-use URL::Shorten::GitHub;
+
+use URL::Shorten qw(makeashorterlink);
+use utf8;
+
+# TEST WITH: http://en.wikipedia.org/wiki/List_of_Internet_top-level_domains#Internationalized_country_code_top-level_domains
 
 
+my @urls = (
+    'http://www.google.com/asdf?quest=asdf#sdfsad',
+    'http://✪df.ws',
+    '✪df.ws',
+    'müller.example.org',
+    'http://例子.測試/首頁',
+    '例子.測試/首頁',
+    '例子.測試',
+    'www.google.com/asdf?quest=asdf#sdfsad',
+    'user:pass@www.google.com/asdf?quest=asdf#sdfsad',
+    'https://user:pass@www.google.com/asdf?quest=asdf#sdfsad',
+    'http://www.google.com:999/asdf?quest=asdf#sdfsad',
+    'www.google.com:999/asdf?quest=asdf#sdfsad',
+    'https://metacpan.org/module/Moo::Role',
+    'apple',
+    'apple.com:80',
+);
 
 
+for my $url (@urls) {
+   my $short = URL::Shorten->new();
+   $short->url($url);
+
+    say 'Original: ' . $url;
+    say 'OldSkool: ' . makeashorterlink($url);
+    say 'NewSkool: ' . $short->shorten;
+
+    say '----';
+}
 
 
+# Getters / Setters
+# url     => gets and sets the original "long" url
+#
+# Eventually will have more, and custom ones for authentication etc
+
+
+# Methods
+# shorten   => shortens the url. Returns original URL on failure, and carps
+# unshorten => checks the url, and reutrns the longer original version.
+
+
+1;
--- lib/URL/Shorten.pm
+++ lib/URL/Shorten.pm
@@ -2,8 +2,78 @@ package URL::Shorten;
 
 # ABSTRACT: Shorten URLs using many URL shorteners
 
-use strict;
-use warnings;
+use common::sense;
+use utf8;
+use Moo;
+
+use URI;
+use URI::Heuristic qw(uf_uri);
+
+use Carp;
+
+use Exporter 'import';
+our @EXPORT_OK = qw(makeashorterlink makealongerlink);
+
+
+has url => (
+    is     => 'rw',
+    coerce => \&_into_url,
+    isa    => \&_url,
+);
+
+
+sub shorten {
+    my $self = shift;
+
+    return $self->url;
+}
+
+sub unshorten {
+    my $self = shift;
+}
+
+
+
+
+
+
+sub makeashorterlink {
+    return URL::Shorten->new({
+        url => shift,
+    })->shorten;
+}
+
+sub makealongerlink {
+    return URL::Shorten->new({
+        url => shift,
+    })->unshorten;
+}
+
+
+sub _url {
+    my $uri = shift || croak 'No URL specified';
+
+    # Coerce gives everything a scheme,
+    # but to be a sane URL we need more than just a scheme
+    if (!$uri->scheme || !$uri->opaque) {
+        croak "URL provided doesn't appear valid. Was given: $uri";
+    }
+
+    return $uri->canonical;
+}
+
+sub _into_url {
+    my $uri = uf_uri $_[0];
+
+    # uf_uri percent encodes URIs that have no scheme and start with a unicode
+    # character and doesn't add the default http scheme
+    if (URI::eq($uri, $_[0]) && !$uri->scheme) {
+        return uf_uri('http://' . $_[0]);
+    }
+
+    return $uri;
+}
+
 
 1;
 
