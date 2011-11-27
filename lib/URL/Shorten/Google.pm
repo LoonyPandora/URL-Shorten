b46af0df11747f7db3709eb77b3881ba0f6d55dd
James Aitken <loonypandora@gmail.com>
2011-11-27 09:55:05 +0000

    Add Goo.gl shortener

--- lib/URL/Shorten/Google.pm
+++ lib/URL/Shorten/Google.pm
@@ -7,12 +7,32 @@ use strict;
 
 use Moo;
 use Carp;
+use HTTP::Request;
+use JSON;
 
 with 'URL::Shorten';
 
 
 sub shorten {
-    carp "Unimplemented";
+    my $self = shift;
+
+    my $request = HTTP::Request->new;
+    $request->method('POST');
+    $request->uri('https://www.googleapis.com/urlshortener/v1/url');
+    $request->content_type('application/json');
+    $request->content( encode_json({ longUrl => $self->url->as_string }) );
+
+    $self->response(
+        $self->ua->request($request)
+    );
+
+    if ($self->response->is_success) {
+        my $content = decode_json $self->response->content;
+
+        return $content->{id};
+    }
+
+    return $self->url;
 }
 
 
