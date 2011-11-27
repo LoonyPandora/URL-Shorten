354067c602537952ded575cc1046cf26c0554cc3
James Aitken <loonypandora@gmail.com>
2011-11-27 03:57:02 +0000

    Add TinyURL shortener

---
+++ lib/URL/Shorten/TinyURL.pm
@@ -0,0 +1,31 @@
+package URL::Shorten::TinyURL;
+
+# ABSTRACT: Shorten GitHub URLs using GitHub's URL shortener - git.io
+
+use utf8;
+use strict;
+
+use Moo;
+use Carp;
+
+with 'URL::Shorten';
+
+
+sub shorten {
+    my $self = shift;
+
+    $self->response(
+        $self->ua->post('http://tinyurl.com/api-create.php', {
+            url => $self->url->as_string,
+        })
+    );
+
+    if ($self->response->is_success && $self->response->content !~ /^Error$/) {
+        return $self->response->content;
+    }
+
+    return $self->url;
+}
+
+
+1;
