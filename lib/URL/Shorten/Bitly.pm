c26b47916448e3fe917302ee8a60bd0e09f357fb
James Aitken <loonypandora@gmail.com>
2011-11-30 15:12:55 +0000

    Flesh out the rest of the bit.ly API

--- lib/URL/Shorten/Bitly.pm
+++ lib/URL/Shorten/Bitly.pm
@@ -13,23 +13,14 @@ use URI;
 with 'URL::Shorten';
 
 
-has apikey => (
-    is       => 'rw',
-    isa      => \&_valid_api_key,
-    required => 1,
-);
-
-has login => (
-    is       => 'rw',
-    isa      => \&_valid_login,
-    required => 1,
-);
-
-has domain => (
-    is       => 'rw',
-    isa      => \&_accepted_domain,
-    default  => sub { 'bit.ly' },
-);
+has apikey   => ( is => 'rw', isa => \&_valid_api_key,   required => 1 );
+has login    => ( is => 'rw', isa => \&_valid_login,     required => 1 );
+has domain   => ( is => 'rw', isa => \&_accepted_domain, default  => sub { 'bit.ly' } );
+has days     => ( is => 'rw', isa => \&_valid_day,       default  => sub { 7; }       );
+has format   => ( is => 'ro', isa => \&_valid_format,    default  => sub { 'json'; }  );
+has hash     => ( is => 'rw', isa => \&_valid_hash    );
+has x_apikey => ( is => 'rw', isa => \&_valid_api_key );
+has x_login  => ( is => 'rw', isa => \&_valid_login   );
 
 
 sub shorten {
@@ -41,6 +32,7 @@ sub shorten {
             apiKey  => $self->apikey,
             longUrl => $self->url->as_string,
             domain  => $self->domain,
+            format  => $self->format,
         })
     );
 
@@ -48,7 +40,7 @@ sub shorten {
         my $content = decode_json $self->response->content;
 
         # Bitly responds with a 200 OK even if there is an error
-        # And puts the REAL error code in the JSON.
+        # And puts the real error code in the JSON.
         if ($content->{status_code} != '200') {
             return $self->url;
         }
@@ -60,45 +52,106 @@ sub shorten {
 }
 
 
+sub expand {
+    my $self = shift;
+
+    $self->_call_api_and_return('http://api.bitly.com/v3/expand', {
+        shortUrl => $self->url->as_string,
+    });
+}
+
+sub validate {
+    my $self = shift;
+
+    $self->_call_api_and_return('http://api.bitly.com/v3/validate', {
+        x_login  => $self->x_login,
+        x_apiKey => $self->x_apikey,
+    });
+}
+
 sub clicks {
     my $self = shift;
 
-    my $endpoint = URI->new('http://api.bitly.com/v3/clicks');
-
-    $endpoint->query_form({
-        login    => $self->login,
-        apiKey   => $self->apikey,
+    $self->_call_api_and_return('http://api.bitly.com/v3/clicks', {
         shortUrl => $self->url->as_string,
     });
-
-    $self->response(
-        $self->ua->get($endpoint)
-    );
-
-    if ($self->response->is_success) {
-        my $content = decode_json $self->response->content;
-
-        if ($content->{status_code} != '200') {
-            return undef;
-        }
-
-        return $content->{data}->{clicks};
-    }
-
-    return $self->url;
 }
-
 
 sub referrers {
     my $self = shift;
 
-    my $endpoint = URI->new('http://api.bitly.com/v3/referrers');
-
-    $endpoint->query_form({
-        login    => $self->login,
-        apiKey   => $self->apikey,
+    $self->_call_api_and_return('http://api.bitly.com/v3/referrers', {
         shortUrl => $self->url->as_string,
     });
+}
+
+sub countries {
+    my $self = shift;
+
+    $self->_call_api_and_return('http://api.bitly.com/v3/countries', {
+        shortUrl => $self->url->as_string,
+    });
+}
+
+sub clicks_by_minute {
+    my $self = shift;
+
+    $self->_call_api_and_return('http://api.bitly.com/v3/clicks_by_minute', {
+        shortUrl => $self->url->as_string,
+    });
+}
+
+sub clicks_by_day {
+    my $self = shift;
+
+    $self->_call_api_and_return('http://api.bitly.com/v3/countries', {
+        shortUrl => $self->url->as_string,
+        days     => $self->days,
+    });
+}
+
+sub bitly_pro_domain {
+    my $self = shift;
+
+    $self->_call_api_and_return('http://api.bitly.com/v3/bitly_pro_domain', {
+        domain => $self->domain,
+    });
+}
+
+sub lookup {
+    my $self = shift;
+
+    $self->_call_api_and_return('http://api.bitly.com/v3/lookup', {
+        url => $self->url->as_string,
+    });
+}
+
+sub info {
+    my $self = shift;
+
+    $self->_call_api_and_return('http://api.bitly.com/v3/info', {
+        shortUrl => $self->url->as_string,
+    });
+}
+
+
+
+
+sub _call_api_and_return {
+    my ($self, $endpoint, $extra_params) = @_;
+
+    $endpoint = URI->new($endpoint);
+
+    $endpoint->query_form({
+        format  => $self->format,
+        login   => $self->login,
+        apiKey  => $self->apikey,
+    });
+
+    # Add extra parameters specific to this endpoint
+    for my $param (keys %$extra_params) {
+        $endpoint->query_param( $param => $extra_params->{$param}) ;
+    }
 
     $self->response(
         $self->ua->get($endpoint)
@@ -107,16 +160,17 @@ sub referrers {
     if ($self->response->is_success) {
         my $content = decode_json $self->response->content;
 
+        # Bitly responds with a 200 OK even if there is an error
+        # putting the real error code in the JSON.
         if ($content->{status_code} != '200') {
             return undef;
         }
 
-        return $content->{data}->{referrers};
+        return $content->{data};
     }
 
     return $self->url;
 }
-
 
 
 sub _valid_api_key {
@@ -128,11 +182,36 @@ sub _valid_api_key {
 }
 
 sub _valid_login {
-    my $login = shift || croak 'No Login specified';
+    my $login = shift || croak 'No Login key specified';
 
     if ($login !~ /^[-\w]+$/) {
         croak "Login key provided doesn't appear valid. Given: $login";
     }
+}
+
+sub _valid_hash {
+    my $hash = shift || croak 'No Hash specified';
+
+    if ($hash !~ /^[-\w]+$/) {
+        croak "Login key provided doesn't appear valid. Given: $hash";
+    }
+}
+
+sub _valid_day {
+    my $day = shift || croak 'No Day specified';
+
+    if ($day < 1 || $day > 30) {
+        croak "Day must be between 1-30. Given: $day";
+    }
+}
+
+sub _valid_format {
+    my $format = shift || croak 'No Format specified';
+
+    my @accepted_formats = qw( json xml txt );
+
+    croak "Invalid format specified. Given: $format"
+        unless grep $_ eq $format, @accepted_formats;
 }
 
 sub _accepted_domain {
@@ -142,10 +221,9 @@ sub _accepted_domain {
     # See: http://code.google.com/p/bitly-api/wiki/ApiDocumentation#/v3/shorten
     my @accepted_domains = qw( bit.ly j.mp bitly.com );
 
-    # Could use smart match, but want compatibility <v5.10
-    croak "Invalid domain specified. Given: $domain" unless grep $_ eq $domain, @accepted_domains;
+    croak "Invalid domain specified. Given: $domain"
+        unless grep $_ eq $domain, @accepted_domains;
 }
-
 
 
 1;
