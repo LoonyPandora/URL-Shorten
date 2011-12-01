62173bf2fdf0abe518458619aaab5351adf8fe8a
James Aitken <loonypandora@gmail.com>
2011-12-01 01:53:04 +0000

    Make API Key required, as google highly recommends it

--- lib/URL/Shorten/Google.pm
+++ lib/URL/Shorten/Google.pm
@@ -13,19 +13,17 @@ use URI;
 
 with 'URL::Shorten';
 
-has key => (
-    is  => 'rw',
-    isa => \&_valid_key,
-);
+
+has key        => ( is  => 'rw', isa => \&_valid_key, required => 1 );
+has projection => ( is  => 'rw', isa => \&_accepted_projection, default => sub { 'FULL' } );
 
 
 sub shorten {
     my $self = shift;
 
-    # Google will most likely require the use of an API key when Goo.gl
-    # exits from lab / beta status. Until then, it's optional
     my $endpoint = URI->new('https://www.googleapis.com/urlshortener/v1/url');
-    $endpoint->query_param( key => $self->key ) if $self->key;
+
+    $endpoint->query_form({ key => $self->key });
 
     $self->response(
         $self->ua->post($endpoint,
@@ -36,14 +34,42 @@ sub shorten {
         )
     );
 
-    if ($self->response->is_success) {
-        my $content = decode_json $self->response->content;
-
-        return $content->{id};
+    if ($self->response->is_error) {
+        croak "Error from Google API: " . $self->response->status_line;
     }
 
-    return $self->url;
+    my $content = decode_json $self->response->content;
+
+    return $content->{id};
 }
+
+
+
+sub analytics {
+    my $self = shift;
+
+    my $endpoint = URI->new('https://www.googleapis.com/urlshortener/v1/url');
+
+    $endpoint->query_form({
+        projection => $self->projection,
+        shortUrl   => $self->url->as_string,
+        key        => $self->key,
+    });
+
+    $self->response(
+        $self->ua->get($endpoint)
+    );
+
+    if ($self->response->is_error) {
+        croak "Error from Google API: " . $self->response->status_line;
+    }
+
+    my $content = decode_json $self->response->content;
+
+    return $content;
+}
+
+
 
 
 sub _valid_key {
@@ -52,6 +78,15 @@ sub _valid_key {
     if ($key !~ /^[-\w]{39}$/) {
         croak "API key provided doesn't appear valid. Given: $key";
     }
+}
+
+sub _accepted_projection {
+    my $projection = shift || croak 'No Projection specified';
+
+    my @accepted_projections = qw( FULL ANALYTICS_CLICKS ANALYTICS_TOP_STRINGS );
+
+    croak "Invalid projection specified. Given: $projection"
+        unless grep $_ eq $projection, @accepted_projections;
 }
 
 
